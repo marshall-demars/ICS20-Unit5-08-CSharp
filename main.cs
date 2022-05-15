2ac223c4c48f38d7fcc87b0feca5666483c3dcb6
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-15 18:24:01 +0000

    main

--- main.cs
+++ main.cs
@@ -1,7 +1,68 @@
+// Created by: Marshall Demars
+// Created on: May 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+                // This function does division
+        int firstInteger;
+        int secondInteger;
+        int remainder;
+        int total = 0;
+
+        // input
+        Console.WriteLine("This program does long division");
+
+        Console.WriteLine("");
+        Console.Write("Please enter the first integer: ");
+        firstInteger = Convert.ToInt32(Console.ReadLine());
+        remainder = firstInteger;
+
+        Console.WriteLine("");
+        Console.Write("Please enter the second integer: ");
+        secondInteger = Convert.ToInt32(Console.ReadLine());
+
+        // process
+        if ((firstInteger > 0) && (secondInteger > 0))
+        {
+            while (remainder >= secondInteger)
+            {
+                remainder = remainder - secondInteger;
+                total++;
+            }
+        }
+        else if ((firstInteger < 0) && (secondInteger < 0))
+        {
+            remainder = Math.Abs(remainder);
+            secondInteger = Math.Abs(secondInteger);
+            while (remainder >= secondInteger)
+            {
+                remainder = remainder - secondInteger;
+                total++;
+            }
+
+            secondInteger = -Math.Abs(secondInteger);
+        }
+        else
+        {
+            remainder = Math.Abs(remainder);
+            secondInteger = Math.Abs(secondInteger);
+            while (remainder >= secondInteger)
+            {
+                remainder = remainder - secondInteger;
+                total++;
+            }
+
+            total = -Math.Abs(total);
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine(firstInteger + " ÷ " + secondInteger + " = " + total + " R " + remainder);
+        Console.WriteLine("\nDone.");
+    }
 }
